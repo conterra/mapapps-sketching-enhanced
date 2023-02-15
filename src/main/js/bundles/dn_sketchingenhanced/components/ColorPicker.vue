65c738eba354783713df469f5404741d2a9c477d
Matthias Stein <m.stein@conterra.de>
2023-02-15 12:32:22 +0100

    Reformat ColorPicker.vue

--- src/main/js/bundles/dn_sketchingenhanced/components/ColorPicker.vue
+++ src/main/js/bundles/dn_sketchingenhanced/components/ColorPicker.vue
@@ -22,17 +22,16 @@
         offset-y
         full-width
     >
-        <template v-slot:activator="{ on }">
+        <template #activator="{ on }">
             <v-btn
                 small
                 block
                 :style="`background-color: ${backgroundColor}`"
                 class="ma-0"
                 v-on="on"
-            >
-            </v-btn>
+            />
         </template>
-        <sketch-picker v-model="pickerColor"></sketch-picker>
+        <sketch-picker v-model="pickerColor" />
     </v-menu>
 </template>
 

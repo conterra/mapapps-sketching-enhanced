54482063da74dcec2e628b197b7693c06ec1b957
Matthias Stein <m.stein@conterra.de>
2023-03-08 18:09:40 +0100

    Move snapping settings to tabs

--- src/main/js/bundles/dn_sketchingenhanced/SketchingEnhancedWidget.vue
+++ src/main/js/bundles/dn_sketchingenhanced/SketchingEnhancedWidget.vue
@@ -57,37 +57,7 @@
                 <v-icon>delete</v-icon>
             </v-btn>
             <v-spacer />
-            <v-menu
-                offset-x
-                :close-on-content-click="false"
-            >
-                <template #activator="{ on }">
-                    <v-btn
-                        flat
-                        color="secondary"
-                        class="ct-flex-item"
-                        v-on="on"
-                    >
-                        {{ i18n.settings }}
-                        <v-icon right>
-                            icon-drawing-settings
-                        </v-icon>
-                    </v-btn>
-                </template>
-                <v-card class="pa-2 dn_sketchingenhanced--settings-menu">
-                    <div class="title mb-2">
-                        {{ i18n.settings }}
-                    </div>
-                    <snapping-settings
-                        :i18n="i18n.snappingSettings"
-                        :snapping-enabled.sync="snappingEnabled"
-                        :snapping-feature-enabled.sync="snappingFeatureEnabled"
-                        :snapping-self-enabled.sync="snappingSelfEnabled"
-                        :snapping-feature-sources.sync="snappingFeatureSources"
-                        @feature-source-changed="$emit('feature-source-changed', $event)"
-                    />
-                </v-card>
-            </v-menu>
+            <!-- TODO: Add Delete-All button-->
         </v-toolbar>
         <div class="ct-flex-container ct-flex-container--row">
             <div class="left ct-flex-item ct-flex-item--no-grow ct-flex-item--no-shrink overflowAuto">
@@ -99,20 +69,26 @@
                     @activate-tool="$emit('activate-tool', $event)"
                 />
             </div>
-            <div class="center ct-flex-item overflowAuto px-3">
+            <div class="center ct-flex-item px-3">
                 <v-tabs
                     slider-color="primary"
+                    grow
                 >
                     <v-tab
                         ripple
                     >
-                        Zeichnen
+                        {{ i18n.tabs.sketching }}
+                    </v-tab>
+                    <v-tab
+                        ripple
+                    >
+                        {{ i18n.tabs.snapping }}
                     </v-tab>
                     <v-tab
                         v-if="measurementWidget && activeUi !== 'text'"
                         ripple
                     >
-                        Messen
+                        {{ i18n.tabs.measurement }}
                     </v-tab>
                     <v-tab-item>
                         <div v-if="!editEnabled">
@@ -151,6 +127,16 @@
                                 :text-symbol.sync="editSymbol"
                             />
                         </div>
+                    </v-tab-item>
+                    <v-tab-item>
+                        <snapping-settings
+                            :i18n="i18n.snappingSettings"
+                            :snapping-enabled.sync="snappingEnabled"
+                            :snapping-feature-enabled.sync="snappingFeatureEnabled"
+                            :snapping-self-enabled.sync="snappingSelfEnabled"
+                            :snapping-feature-sources.sync="snappingFeatureSources"
+                            @feature-source-changed="$emit('feature-source-changed', $event)"
+                        />
                     </v-tab-item>
                     <v-tab-item v-if="measurementWidget && activeUi !== 'text'">
                         <component
